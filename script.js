61fa2dc42f7af499715f74d403d9baa220d9da94
Enzo Cornaggia <enzo.cornaggia@etu.u-bordeaux.fr>
2022-11-21 03:58:22 +0100

    salaires

---
+++ script.js
@@ -0,0 +1,20 @@
+function display_salaire(x) {
+    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/earn_ses_agt21?format=JSON&lang=EN')
+    .then(res => res.json())
+    .then(data => {
+        result3.textContent = `${data.value[x]}€/mois`;
+        // increment index to load the correct value in the JSON
+        x = x + 34;
+        result1.textContent = `${data.value[x]}€/mois`;
+        x = x + 34;
+        result2.textContent = `${data.value[x]}€/mois`;
+    })
+}
+
+result1.textContent = `0€/mois`;
+result2.textContent = `0€/mois`;
+result3.textContent = `0€/mois`;
+var el = document.getElementById('country-select');
+el.addEventListener('change', function() {
+    display_salaire(parseInt(this.value));
+}, false);
