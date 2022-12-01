33e19505658c29ae77539d51498b2575d3c5b9e1
Enzo Cornaggia <enzo.cornaggia@etu.u-bordeaux.fr>
2022-12-01 01:30:38 +0100

    add robust and optimized modification

--- comparateur.js
+++ comparateur.js
@@ -1,216 +1,115 @@
 /*********************** FOR MEDIAN SALARY ***********************/
+let jsondata_sal = {};
+const country_name_sal = new Map();
+const sal_id = new Map();
 
-function catch_salary() {
-    var salary1 = [];
-    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/earn_ses_agt21?format=JSON&lang=EN&sex=T&indic_se=ERN&isco88=TOTAL&age=TOTAL&unit=EUR')
+function catch_salaire () {
+    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/earn_ses_agt21?format=JSON&lang=FR&sex=T&indic_se=ERN&isco88=TOTAL&age=TOTAL&unit=EUR')
     .then(res => res.json())
     .then(data => {
-        for (const i in data.value) {
-            // console.log(`${i}: ${data.value[i]}`);
-            salary1.push(data.value[i]);
-        }
+        set_codename (data, country_name_sal);
+        set_index (data, sal_id);
+        jsondata_sal = data;
     });
-    return salary1;
-}
-
-function display_salary(array, x, index_select) {
-    if (index_select == 1) {
-        sal1.textContent = `${(array[x])}€/mois`;
-    }
-    if (index_select == 2) {
-        sal2.textContent = `${array[x]}€/mois`;
-    }
 }
 
 /************************ FOR POPULATION *************************/
+let jsondata_pop = {};
+const country_name_pop = new Map();
+const id_pop = new Map();
 
 function catch_pop() {
-    var population1 = [];
-    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/tps00001?format=JSON&lang=EN&Time=2021')
+    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/tps00001?format=JSON&lang=FR&Time=2020')
     .then(res => res.json())
     .then(data => {
-        // console.log(data);
-        for (const i in data.value) {
-            // console.log(`${i}: ${data.value[i]}`);
-            population1.push(data.value[i]);
-        }
-        // console.log(population1);
+        set_codename (data, country_name_pop);
+        set_index (data, id_pop);
+        jsondata_pop = data;
     });
-    return population1;
-}
-
-function display_pop(array, x, index_select) {
-    if (index_select == 1) {
-        // console.log(x);
-        pop1.textContent = `${(array[x] / 1000000).toFixed(2)} millions`;
-    }
-    if (index_select == 2) {
-        pop2.textContent = `${(array[x] / 1000000).toFixed(2)} millions`;
-    }
 }
 
 /**************************** FOR PIB ***************************/
 
+let jsondata_pib = {};
+const country_name_pib = new Map();
+const id_pib = new Map();
+
 function catch_pib() {
-    var pib1 = [];
-    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/NAMA_10_GDP?format=JSON&lang=EN&time=2019&unit=CP_MEUR&na_item=B1GQ')
+    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/tps00001?format=JSON&lang=FR&Time=2020')
     .then(res => res.json())
     .then(data => {
-        // console.log(data);
-        for (const i in data.value) {
-            // console.log(`${i}: ${data.value[i]}`);
-            pib1.push(data.value[i]);
-        }
-        // console.log(pib1);
-    })
-    return pib1;
-}
-
-function display_pib(array, x , index_select) {
-    if (index_select == 1) {
-        pib1.textContent = `${(array[x] / 1000).toFixed(2)} milliards €`;
-    }
-    if (index_select == 2) {
-        pib2.textContent = `${(array[x] / 1000).toFixed(2)} milliards €`;
-    }
+        set_codename (data, country_name_pib);
+        set_index (data, id_pib);
+        jsondata_pib = data;
+    });
 }
 
 /**************************** FOR LIFE **************************/
 
+let jsondata_life = {};
+const country_name_life = new Map();
+const id_life = new Map();
+
 function catch_life() {
-    var life1 = [];
-    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/DEMO_MLEXPEC?format=JSON&lang=EN&time=2018')
+    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/DEMO_MLEXPEC?format=JSON&lang=FR&time=2018')
     .then(res => res.json())
     .then(data => {
-        // console.log(data);
-        for (const i in data.value) {
-            // console.log(`${i}: ${data.value[i]}`);
-            life1.push(data.value[i]);
-        }
-        // console.log(life1);
-    })
-    return life1;
-}
-
-function display_life(array, x , index_select) {
-    if (index_select == 1) {
-        vie1.textContent = `${array[x]} ans`;
-    }
-    if (index_select == 2) {
-        vie2.textContent = `${array[x]} ans`;
-    }
+        set_codename (data, country_name_life);
+        set_index (data, id_life);
+        jsondata_life = data;
+    });
 }
 
 /************************** FOR EDUCATION ************************/
 
-function catch_education() {
-    var edu1 = [];
-    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/EDUC_UOE_FINE04?format=JSON&lang=EN&time=2018&isced11=ED5-8&unit=MIO_EUR')
+let jsondata_edu = {};
+const country_name_edu = new Map();
+const id_edu = new Map();
+
+function catch_edu() {
+    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/EDUC_UOE_FINE04?format=JSON&lang=FR&time=2018&isced11=ED5-8&unit=MIO_EUR')
     .then(res => res.json())
     .then(data => {
-        // console.log(data);
-        for (const i in data.value) {
-            // console.log(`${i}: ${data.value[i]}`);
-            edu1.push(data.value[i]);
-        }
-        // console.log(edu1);
-    })
-    return edu1;
-}
-
-function display_education(array, x , index_select) {
-    if (index_select == 1) {
-        edu1.textContent = `${array[x].toLocaleString()} millions €`;
-    }
-    if (index_select == 2) {
-        edu2.textContent = `${array[x].toLocaleString()} millions €`;
-    }
+        set_codename (data, country_name_edu);
+        set_index (data, id_edu);
+        jsondata_edu = data;
+    });
 }
 
 /*************************** FOR MEDICAL *************************/
 
-function catch_medical() {
-    var med1 = [];
-    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/TPS00207?format=JSON&lang=EN&time=2019&unit=MIO_EUR')
+let jsondata_med = {};
+const country_name_med = new Map();
+const id_med = new Map();
+
+function catch_med() {
+    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/TPS00207?format=JSON&lang=FR&time=2019&unit=MIO_EUR')
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-        for (const i in data.value) {
-            // console.log(`${i}: ${data.value[i]}`);
-            med1.push(data.value[i]);
-        }
-        console.log(med1);
-    })
-    return med1;
-}
-
-function display_medical(array, x , index_select) {
-    if (index_select == 1) {
-        med1.textContent = `${array[x].toLocaleString()} millions €`;
-    }
-    if (index_select == 2) {
-        med2.textContent = `${array[x].toLocaleString()} millions €`;
-    }
+        set_codename (data, country_name_med);
+        set_index (data, id_med);
+        jsondata_med = data;
+    });
 }
 
 
-function convert_index_population(x) {
-    if (x > 1 && x < 16) {
-        x = x - 2;
+function set_codename (data, map) {
+    for (const i in data.dimension.geo.category.label) {
+        map.set(data.dimension.geo.category.label[i], i);
     }
-    else if (x == 33) {
-        x = x + 3;
-    }
-    else if (x == 34) {
-        x = x + 2;
-    }
-    return x;
 }
 
-function convert_index_pib(x) {
-    if (x > 1 && x < 15){
-        x = x + 1;
+function set_index (data, map) {
+    for (const i in data.dimension.geo.category.index) {
+        map.set(i, data.dimension.geo.category.index[i]);
     }
-    else if (x >= 15 && x < 32){
-        x = x + 2;
-    }
-    else if (x == 32) {
-        x = x + 3;
-    }
-    else if (x == 33) {
-        x = x + 4;
-    }
-    return x;
 }
 
-function convert_index_edu(x) {
-    // console.log(x);
-    if (x > 1 && x < 15){
-        x = x - 3;
-    }
-    else if (x >= 15 && x < 33){
-        x = x - 2;
-    }
-    else if (x == 33) {
-        x = x - 1;
-    }
-    // console.log(x);
-    return x;
-}
 
-function convert_index_med(x) {
-    console.log(x);
-    if (x >= 15 && x < 32){
-        x = x + 1;
-    }
-    else if (x == 32) {
-        x = x + 2;
-    }
-    else if (x == 33) {
-        x = x + 3;
-    }
-    console.log(x);
-    return x;
+function display(data, x, map1, map2, id, format) { // data = JSON, x = select value (string), id = id for add content HTML
+    let codename = map1.get(x);
+    let values = map2.get(codename);
+    document.getElementById(id).textContent = format.replace("{}", data.value[values].toLocaleString());
 }
 
 pop1.textContent = `---`;
@@ -226,30 +125,31 @@ med2.textContent = `---`;
 vie1.textContent = `---`;
 vie2.textContent = `---`;
 
-var el1 = document.getElementById('country1');
-var array_salary = catch_salary();
-var array_population = catch_pop();
-var array_pib = catch_pib();
-var array_life = catch_life();
-var array_education = catch_education();
-var array_medical = catch_medical();
 
-console.log(array_population);
+catch_salaire();
+catch_pop();
+catch_pib();
+catch_life();
+catch_med();
+catch_edu();
+
+var el1 = document.getElementById('country1');
 el1.addEventListener('change', function() {
-    display_salary(array_salary, parseInt(this.value), 1);
-    display_pop(array_population, convert_index_population(parseInt(this.value)), 1);
-    display_pib(array_pib, convert_index_pib(parseInt(this.value)), 1);
-    display_life(array_life, convert_index_pib(parseInt(this.value)), 1);
-    display_education(array_education, convert_index_edu(parseInt(this.value)), 1);
-    display_medical(array_medical, convert_index_med(parseInt(this.value)), 1);
+    display (jsondata_sal, this.value, country_name_sal, sal_id, "sal1", "{} €/mois");
+
+    display(jsondata_pop, this.value, country_name_pop, id_pop, "pop1", "{} habitants");
+    display(jsondata_pib, this.value, country_name_pib, id_pib, "pib1", "{} millions");
+    display(jsondata_life, this.value, country_name_life, id_life, "vie1", "{} ans");
+    display(jsondata_edu, this.value, country_name_edu, id_edu, "edu1", "{} millions");
+    display(jsondata_med, this.value, country_name_med, id_med, "med1", "{} millions");
 }, false);
 
 var el2 = document.getElementById('country2');
 el2.addEventListener('change', function() {
-    display_salary(array_salary, parseInt(this.value), 2);
-    display_pop(array_population, convert_index_population(parseInt(this.value)), 2);
-    display_pib(array_pib, convert_index_pib(parseInt(this.value)), 2);
-    display_life(array_life, convert_index_pib(parseInt(this.value)), 2);
-    display_education(array_education, convert_index_edu(parseInt(this.value)), 2);
-    display_medical(array_medical, convert_index_med(parseInt(this.value)), 2);
+    display (jsondata_sal, this.value, country_name_sal, sal_id, "sal2", "{} €/mois");
+    display(jsondata_pop, this.value, country_name_pop, id_pop, "pop2", "{} habitants");
+    display(jsondata_pib, this.value, country_name_pib, id_pib, "pib2", "{} millions");
+    display(jsondata_life, this.value, country_name_life, id_life, "vie2", "{} ans");
+    display(jsondata_edu, this.value, country_name_edu, id_edu, "edu2", "{} millions");
+    display(jsondata_med, this.value, country_name_med, id_med, "med2", "{} millions");
 }, false);
--- test.js
+++ test.js
@@ -1,33 +1,54 @@
-function display_salaire(x, index_select) {
-    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/TEC00001?format=JSON&lang=EN&Time=2018')
+/* function catch_salaire(name, index_select) {
+    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/TEC00001?format=JSON&lang=FR&Time=2018')
     .then(res => res.json())
     .then(data => {
-        var result = make_array(data);
-        
+        console.log(data);
+        set_codename(data);
+        set_index(data);
+        display_salaire(data, name)
     })
 }
 
-
-
-function make_array(data) {
-    var result = [];
-    for (var i in data.value) {
-        result.push(data.value[i]);
+function set_codename (data) {
+    for (const i in data.dimension.geo.category.label) {
+        country_name.set(data.dimension.geo.category.label[i], i);
     }
-    console.log(result);
-    return result;
 }
 
-function do_index(data) {
-    var i = 0;
-    data.forEach(element => {
-        country_id[i] = data.dimension.geo.category.index.country[i];
-        i++;
-    });
-    console.log("ok");
+function set_index (data) {
+    for (const i in data.dimension.geo.category.index) {
+        //console.log(i);
+        id.set(i, data.dimension.geo.category.index[i]);
+    }
 }
 
-display_salaire(0,0);
+function display_salaire (data, x) {
+    var codename;
+    country_name.forEach(function(values, keys) {
+        if (keys == x) {
+            codename = values;
+        }
+    })
+    id.forEach(function(values, keys) {
+        if (codename == keys) {
+            display.textContent = `${data.value[values]} €`;
+        }
+    })
+}
+
+const country_name = new Map();
+const id = new Map();
+
+catch_salaire(0,0);
+
+console.log(country_name);
+console.log(id);
+
+var el1 = document.getElementById('country1');
+el1.addEventListener('change', function() {
+    catch_salaire(this.value, 0);
+}, false);
+
 
 let country = ['AL', 'AT', 'BE', 'BG', 'CH', 'CY', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES', 'FI', 'FR', 'HR', 'HU', 'IE', 'IS', 'IT', 'LT', 'LU', 'MT', 'NL', 'NO', 'PL', 'PT', 'RO', 'RS', 'SE', 'SI', 'SK', 'TR', 'UK'];
 let country_id = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -65,3 +86,63 @@ var SK= "Slovakia";
 var TR= "Türkiye";
 var UK= "United Kingdom";
 
+ */
+
+/*********************** FOR MEDIAN SALARY ***********************/
+let jsondata = {};
+const country_name = new Map();
+const id = new Map();
+
+function catch_salaire () {
+    fetch('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/earn_ses_agt21?format=JSON&lang=FR&sex=T&indic_se=ERN&isco88=TOTAL&age=TOTAL&unit=EUR')
+    .then(res => res.json())
+    .then(data => {
+        set_codename (data);
+        set_index (data);
+        jsondata = data;
+        console.log("deuxieme then");
+    });
+}
+
+
+function set_codename (data) {
+    for (const i in data.dimension.geo.category.label) {
+        country_name.set(data.dimension.geo.category.label[i], i);
+    }
+}
+
+function set_index (data) {
+    for (const i in data.dimension.geo.category.index) {
+        //console.log(i);
+        id.set(i, data.dimension.geo.category.index[i]);
+    }
+}
+
+
+function display_salaire (data, x) {
+    var codename;
+    country_name.forEach(function(values, keys) {
+        if (keys == x) {
+            codename = values;
+        }
+    })
+    console.log(codename);
+    id.forEach(function(values, keys) {
+        if (codename == keys) {
+            display.textContent = `${data.value[values]} €`;
+        }
+    })
+}
+
+var el1 = document.getElementById('country1');
+
+catch_salaire();
+console.log("Après la fonc");
+console.log("Après la ");
+
+el1.addEventListener('change', function() {
+    console.log(country_name);
+    console.log(this.value);
+    display_salaire (jsondata, this.value);
+    //console.log(this.value);
+}, false);
